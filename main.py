df0f0156a7ef82cf2d35c2943940b3adc69f8692
Francisco Viramontes <fviramontes8@unm.edu>
2022-04-16 01:19:02 -0600

    Update main.py
    Added plot_histogram, but it does not work, I do not recommend calling that function.
    
    Added SVM 2 regressors, one with a linear kernel and one with an RBF kernel. The 5th feature of data was chosen for training and testing. The data was split in an 80/20 ration and three values 0.001, 0.01, and 0.1 are chosen as the free parameter for training 6 models in total. A prediction is made on the dataset and the results are plotted against the test y for evaluation of the performance of the model.
    
    The plot of the linear prediction is initially commented out because it is poor. To see the poor performance, uncomment the line.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 
 from struct import calcsize, Struct
-from numpy import atleast_2d, transpose, clip, isnan
+from numpy import atleast_2d, transpose, clip, histogram
+from math import fabs
+
+from sklearn.svm import SVR
 
 
 def read_source_data(filename, struct_unpacker, struct_length):
@@ -19,6 +22,7 @@ def read_source_data(filename, struct_unpacker, struct_length):
 # Assumes 9 by N matrix where indices 0 - 7 are features and 8 is a time-series of timestamps
 def plot_features(data):
     for i in range(len(data) - 2):
+        plt.title(f"Graph: {i}")
         plt.plot(data[len(data) - 1], data[i])
         plt.show()
 
@@ -26,9 +30,23 @@ def plot_features(data):
 def calculate_timestamp_differences(data):
     timestamp_differences = []
     for i, j in zip(range(len(data) - 1), range(1, len(data))):
-        print(f"i: {i}, j: {j}")
-        timestamp_differences.append(data[i] - data[j])
+        timestamp_differences.append(fabs(data[i] - data[j]))
     return timestamp_differences
+
+
+def plot_histogram(data):
+    for i in range(len(data)):
+        timestamp_differences = calculate_timestamp_differences(data[i])
+        timestamp_differences = timestamp_differences[:500]
+
+        data_histogram, data_bins = histogram(timestamp_differences, bins='auto')
+        print(data_histogram)
+        print(data_bins)
+        plt.hist(data_histogram, data_bins)
+        plt.title(f"Histogram of data[{i}]")
+        plt.ylabel("Frequency")
+        plt.xlabel("Value")
+        plt.show()
 
 
 # Create linear SVM regressor
@@ -49,12 +67,46 @@ def main():
     )
     data = transpose(data)
     for i in range(len(data) - 2):
-        print(isnan(data[i]))
         data[i] = clip(data[i], None, 4)
-    # plot_features(data)
 
-    timestamp_diffs = calculate_timestamp_differences(data[8])
-    print(f"Mean of differences: {sum(timestamp_diffs) / len(timestamp_diffs)}")
+    # plot_histogram(data)
+
+    # plot_features(data)
+    ratio = 0.8
+    cutoff = int(len(data[4]) * ratio)
+    train_x = data[8][:cutoff].reshape(-1, 1)
+    train_y = data[4][:cutoff]
+    test_x = data[8][cutoff + 1:].reshape(-1, 1)
+    test_y = data[4][cutoff + 1:]
+
+    print(f"train_x shape: {train_x.shape}")
+    print(f"train_y shape: {train_y.shape}")
+    print(f"test_x shape: {test_x.shape}")
+    print(f"test_y shape: {test_y.shape}")
+
+    C = [0.001, 0.01, 0.1]
+    for i in C:
+        linear_svm_model = SVR(kernel='linear', C=i)
+        rbf_svm_model = SVR(kernel='rbf', C=i)
+
+        print(f"Training linear SVR")
+        linear_svm_model.fit(train_x, train_y)
+        print(f"Training RBF SVR")
+        rbf_svm_model.fit(train_x, train_y)
+
+        print(f"Testing with Linear SVR")
+        linear_pred = linear_svm_model.predict(test_x)
+        print(f"Testing with RBF SVR")
+        rbf_pred = rbf_svm_model.predict(test_x)
+
+        # print(f"linear prediction shape: {linear_pred.shape}")
+        # print(f"rbf prediction shape: {rbf_pred.shape}")
+        plt.plot(test_x[:, 0], test_y, "k", label="Actual")
+        # plt.plot(test_x[:, 0], linear_pred, "m", label="Linear prediction")
+        plt.plot(test_x[:, 0], rbf_pred, "c", label="RBF prediction")
+        plt.title(f"Plot of Linear SVR and RBF SVR for C = {i}")
+        plt.legend()
+        plt.show()
 
 
 if __name__ == "__main__":
